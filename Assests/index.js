46ed7289354034657f531bc7b962e90f87b3e70e
DELL <rahulmishra0117@gmail.com>
2021-05-09 09:05:30 -1200

    adding the README file

--- Assests/index.js
+++ Assests/index.js
@@ -70,8 +70,22 @@ function deleteTodo(i) {
 }
 
 // complete todo
-function isComplete(e){
-    const checked = e.target.classList.toggle('checked');
-    e.target.tagName === 'SPAN' ? checked: null;
-    console.log(e.target);
-}
+
+
+var textWrapper = document.querySelector(' .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml9 .letter',
+    scale: [0, 1],
+    duration: 1500,
+    elasticity: 600,
+    delay: (el, i) => 45 * (i+1)
+  }).add({
+    targets: '.ml9',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
---
+++ README.md
@@ -0,0 +1,9 @@
+# TODO APP 
+A TODO APP contain the functionlity of Adding some Task ,Editing some task And Deleting some Task form Todo.
+
+## TECH STACK
+  # HTML
+  ## CSS
+  ### JAVASCRIPT
+
+
